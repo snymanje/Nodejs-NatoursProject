210781fea5621961791533dab58185f56a5a9423
snymanje <jean.snyman@gmail.com>
2019-10-31 05:50:17 +0200

    done with error handling

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+//Uncaught Exceptions
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -17,6 +23,9 @@ mongoose
   })
   .then(() => {
     console.log('We are connected to MongoDB Atlas');
+  })
+  .catch(err => {
+    console.log('Error connecting to the DB', err);
   });
 
 const app = require('./app');
@@ -24,6 +33,14 @@ const app = require('./app');
 //START SERVER
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}....`);
+});
+
+//Unhandled Rejections
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
